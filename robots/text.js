664571f752dad9b53d05390c4a6a48c705a036d8
Guilherme Strutzki <guilherme.strutzki@possible.com>
2019-07-05 13:01:52 +0000

    Feat: Add Watson NLU service

--- robots/text.js
+++ robots/text.js
@@ -1,11 +1,21 @@
 const algorithmia = require('algorithmia');
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
 const sentenceBoundaryDetection = require('sbd');
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
+
+var nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
 
 async function robot(content) {
   await fetchContentFromWikipedia(content);
   sanitizeContent(content);
   breakContentIntoSentences(content);
+  limitMaximumSentences(content);
+  await fetchKeyWordsOfAllSentences(content);
 
   async function fetchContentFromWikipedia(content) {
     const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
@@ -52,8 +62,37 @@ async function robot(content) {
         images: []
       });
     })
+  }
 
-    console.log(content.sentences);
+  function limitMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0, content.maximumSentences);
+  }
+
+  async function fetchKeyWordsOfAllSentences(content) {
+    for(const sentence of content.sentences) {
+      sentence.keyword = await fetchWatsonAndReturnKeyWords(sentence.text);
+    }
+  }
+
+  async function fetchWatsonAndReturnKeyWords(sentence) {
+    return new Promise( (resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+        if (error) {
+          throw error
+        }
+      
+        const keywords = response.keywords.map( keyword => {
+          return keyword.text;
+        });
+  
+        resolve(keywords);
+      });
+    });
   }
 }
 
