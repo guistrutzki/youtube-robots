a8a274106ef724144212f0bbb98d1090fe35d22f
Guilherme Strutzki <guilherme.strutzki@possible.com>
2019-07-11 16:32:55 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const readline = require('readline-sync');
 const robots = {
   input: require('./robots/input'),
   text: require('./robots/text'),
+  image: require('./robots/image')
 }
 
 async function start() {
-  robots.input();
-  robots.text();
+  // robots.input();
+  // await robots.text();
+  await robots.image();
 }
 
 start();
